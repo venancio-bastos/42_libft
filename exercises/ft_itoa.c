ce1820136af0aa463f054ec171ad74188533d9bc
venancio-bastos <vjpb2002@gmail.com>
2025-10-24 11:50:38 +0100

    Submit Files

--- exercises/ft_itoa.c
+++ exercises/ft_itoa.c
@@ -1,13 +1,65 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft_itoa.c                                       :+:      :+:    :+:   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vebastos <vebastos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 11:19:19 by vebastos          #+#    #+#             */
-/*   Updated: 2025/10/23 11:19:20 by vebastos         ###   ########.fr       */
+/*   Updated: 2025/10/23 20:17:09 by vebastos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	get_len(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
+	if (n <= 0)
+		len++;
+	while (n != 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	int		i;
+	int		n_len;
+	long	nl;
+	char	*nbr;
+
+	n_len = get_len(n);
+	nbr = malloc(n_len + 1);
+	nl = n;
+	if (!nbr)
+		return (NULL);
+	nbr[n_len] = '\0';
+	i = 0;
+	if (nl < 0)
+	{
+		nbr[i++] = '-';
+		nl *= -1;
+	}
+	while (i < n_len)
+	{
+		nbr[--n_len] = (nl % 10) + '0';
+		nl /= 10;
+	}
+	return (nbr);
+}
+
+/*
+int	main(void)
+{
+	printf("Result: %s", ft_itoa(-1));
+	return (0);
+}
+*/
--- exercises/ft_split.c
+++ exercises/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: vebastos <vebastos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 00:29:23 by vebastos          #+#    #+#             */
-/*   Updated: 2025/10/23 15:15:48 by vebastos         ###   ########.fr       */
+/*   Updated: 2025/10/23 18:35:51 by vebastos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,82 @@ int	get_words(char const *s, char c)
 {
 	int	i;
 	int	total;
-	
+
 	i = 0;
 	total = 0;
 	while (s[i])
 	{
 		if (s[i] != c && (i == 0 || s[i - 1] == c))
-            total++;
-        i++;
+			total++;
+		i++;
 	}
-	return (total );
+	return (total);
 }
 
-// char	**ft_split(char const *s, char c)
-// {
-// 	char	**arr;
+char	*copy_next_word(char const *s, char c, int *i)
+{
+	int		start;
+	int		len;
+	char	*word;
+	int		j;
 
-// 	return (arr);
-// }
+	while (s[*i] == c)
+		(*i)++;
+	start = *i;
+	while (s[*i] && s[*i] != c)
+		(*i)++;
+	len = *i - start;
+	word = malloc(sizeof(char) * (len + 1));
+	if (!word)
+		return (NULL);
+	j = 0;
+	while (j < len)
+	{
+		word[j] = s[start + j];
+		j++;
+	}
+	word[j] = '\0';
+	return (word);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	int		x;
+	char	**arr;
+
+	arr = malloc(sizeof(char *) * (get_words(s, c) + 1));
+	if (!arr)
+		return (NULL);
+	i = 0;
+	x = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] == '\0')
+			break ;
+		arr[x] = copy_next_word(s, c, &i);
+		if (!arr[x])
+			return (NULL);
+		x++;
+	}
+	arr[x] = NULL;
+	return (arr);
+}
+
 
 int	main(void)
 {
-	char const s[100] = "   Hello 42 World";
-	printf("Number of words: %d\n", get_words(s, 32));
+	char const s[100] = "   Hello 42 World       ";
+	char **result = ft_split(s, ' ');
+	int i = 0;
+	while (result[i])
+	{
+		printf("[%s]\n", result[i]);
+		i++;
+	}
+	printf("Number of words: %d\n", get_words(s, ' '));
 	return (0);
 }
+
